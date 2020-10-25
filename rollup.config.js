2c44b2675aae8d008a15d8f532b9cd08fb1a06c1
Samuel Macleod <samuel@macleod.space>
2020-10-25 19:18:16 +0000

    Initial

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
+import preprocess from "svelte-preprocess";
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
