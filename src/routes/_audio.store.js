43839dfd69aadd335f4eb92ce7404ea05d266ad6
Somhairle MacLeòid <samuel@macleod.space>
2021-10-15 11:34:16 +0100

    Update _audio.store.js

--- src/routes/_audio.store.js
+++ src/routes/_audio.store.js
@@ -73,7 +73,7 @@ const audio = {
 
       return {
         ...v,
-        src: v.live ? v.src : "https://live.freshair.radio/airtime_128",
+        src: v.live ? v.src : "https://live.freshair.radio/airtime_128?ts="+Date.now(), // Browser cache-busting
 
         live: true,
         paused: false,
